9fae7e133f7d454ace2ff1120a5ca1fe99f2b932
Felipe Miranda Santos <felipe_miranda92@outlook.com>
2023-08-31 23:13:47 -0300

    Aula sobre Herança, Polimorfismo e encapsulamento

--- POOTurma351BF/POOTurma351BF/main.swift
+++ POOTurma351BF/POOTurma351BF/main.swift
@@ -98,50 +98,50 @@ print(caio.idade)
 print(felipe.idade)
 
 
-class Casa {
-    
-    var quantidadePortas: Int = 6
-    var temChurrasqueira: Bool = true
-    var corCasa: String = "Amarela"
-    
-    func abrirPorta() {
-        print("Abir a porta.")
-    }
-    
-    func abrirJanela() {
-        print("Abrir janela.")
-    }
-}
-
-class Animal {
-    
-    var especie: String = "Inseto"
-    var quantidadeDePatas: Int = 6
-    var voa: Bool = true
-    
-    func comer() {
-        print("O animal come")
-    }
-    
-    func voar() {
-        print("O animal voa")
-    }
-}
-
-class Computador {
-    
-    var modelo: String = "Macbook air"
-    var cor: String = "Prata"
-    var memoriaRam: Int = 16
-    
-    func ligar() {
-        print("O computador está ligando")
-    }
-    
-    func tirarFoto() {
-        print("O computador tira foto")
-    }
-}
+//class Casa {
+//
+//    var quantidadePortas: Int = 6
+//    var temChurrasqueira: Bool = true
+//    var corCasa: String = "Amarela"
+//
+//    func abrirPorta() {
+//        print("Abir a porta.")
+//    }
+//
+//    func abrirJanela() {
+//        print("Abrir janela.")
+//    }
+//}
+//
+//class Animal {
+//
+//    var especie: String = "Inseto"
+//    var quantidadeDePatas: Int = 6
+//    var voa: Bool = true
+//
+//    func comer() {
+//        print("O animal come")
+//    }
+//
+//    func voar() {
+//        print("O animal voa")
+//    }
+//}
+//
+//class Computador {
+//
+//    var modelo: String = "Macbook air"
+//    var cor: String = "Prata"
+//    var memoriaRam: Int = 16
+//
+//    func ligar() {
+//        print("O computador está ligando")
+//    }
+//
+//    func tirarFoto() {
+//        print("O computador tira foto")
+//    }
+//}
 
 // Construtores
 // Os construtores tem a finalidade de indicar oque a sua classe necessita de uma valor inicial para seus atributos.
@@ -177,63 +177,123 @@ class Computador {
 
 // Desafios em Aula
 
-class Televisao {
+//class Televisao {
+//
+//    var polegadas: Double
+//    var imagem: String
+//    var borda: Bool
+//
+//    func ligarTV() {
+//        print("A TV tem uma imagem \(imagem)")
+//    }
+//
+//    func diminuirImagem() {
+//        print("A TV reajustou o tamanho da imagem para modo full, de \(polegadas)")
+//    }
+//
+//    init(polegadas: Double, imagem: String, borda: Bool) {
+//        self.polegadas = polegadas
+//        self.imagem = imagem
+//        self.borda = borda
+//    }
+//}
+//
+//
+//class Estadio {
+//    var nome: String
+//    var capacidade: Int
+//    var tetoRetratil: Bool
+//
+//    func abrirTeto() {
+//        print("O teto retrátil está abrindo \(tetoRetratil)")
+//    }
+//
+//    func lotacaoMaxima() {
+//        print("Estádio \(nome) com lotação máxima de \(capacidade) foi atingido")
+//    }
+//
+//    init(nome: String, capacidade: Int, tetoRetratil: Bool) {
+//        self.nome = nome
+//        self.capacidade = capacidade
+//        self.tetoRetratil = tetoRetratil
+//    }
+//}
+//
+//class Ventilador {
+//    var rpm: Int = 100
+//    var modoTurbo: Bool
+//    var tamanho: Double
+//
+//    func aumentarVelocidade() {
+//        print("O ventilador funciona a \(rpm) rotações por minutos")
+//    }
+//
+//    func deligaModoTurbo() {
+//        print("O modo turbo está ligado? \(modoTurbo)")
+//    }
+//
+//    init(modoTurbo: Bool, tamanho: Double) {
+//        self.modoTurbo = modoTurbo
+//        self.tamanho = tamanho
+//    }
+//}
+
+
+// Criar uma classe pai na qual tenha 3 caracteristicas e 2 ações.
+// Criar 2 classes filhas distintas, porem ambas vão herdar da classe pai(super). Cada classe filha terá 3 caracteristicas especificas.
+// A classe pai terá construtor para setar seus valores.
+// Uma das classes filhas não deve conter construtor.
+// Uma das classes filhas deve conter contrutor indicando os valores de todas as suas propriedades.
+
+class Carro {
     
-    var polegadas: Double
-    var imagem: String
-    var borda: Bool
+    var quantidadeDePortas: Int
+    var sensorDere: Bool
+    var fabricante: String
     
-    func ligarTV() {
-        print("A TV tem uma imagem \(imagem)")
+    func acelerar() {
+        print("O carro está acelerando")
     }
     
-    func diminuirImagem() {
-        print("A TV reajustou o tamanho da imagem para modo full, de \(polegadas)")
+    func ligarMotor() {
+        print("O motor está ligado")
     }
     
-    init(polegadas: Double, imagem: String, borda: Bool) {
-        self.polegadas = polegadas
-        self.imagem = imagem
-        self.borda = borda
+    init(quantidadeDePortas: Int, sensorDere: Bool, fabricante: String) {
+        self.quantidadeDePortas = quantidadeDePortas
+        self.sensorDere = sensorDere
+        self.fabricante = fabricante
     }
 }
 
+class Citroen: Carro {
+    
+    var logoCitroen: String = "Logo Citroen"
+    var potenciaCitroen: Double = 120.0
+    var ligaLeveCitroen: Bool = true
+    
+}
 
-class Estadio {
-    var nome: String
-    var capacidade: Int
-    var tetoRetratil: Bool
+class Pegeout: Carro {
     
-    func abrirTeto() {
-        print("O teto retrátil está abrindo \(tetoRetratil)")
-    }
+    var logoLeao: Bool
+    var painel3D: Bool
+    var revioesConcessionaria: String
     
-    func lotacaoMaxima() {
-        print("Estádio \(nome) com lotação máxima de \(capacidade) foi atingido")
-    }
-    
-    init(nome: String, capacidade: Int, tetoRetratil: Bool) {
-        self.nome = nome
-        self.capacidade = capacidade
-        self.tetoRetratil = tetoRetratil
+    init(logoLeao: Bool, painel3D: Bool, revioesConcessionaria: String, quantidadeDePortas: Int, sensorDere: Bool, fabricante: String) {
+        self.logoLeao = logoLeao
+        self.painel3D = painel3D
+        self.revioesConcessionaria = revioesConcessionaria
+        
+        super.init(quantidadeDePortas: quantidadeDePortas, sensorDere: sensorDere, fabricante: fabricante)
     }
 }
 
-class Ventilador {
-    var rpm: Int = 100
-    var modoTurbo: Bool
-    var tamanho: Double
-    
-    func aumentarVelocidade() {
-        print("O ventilador funciona a \(rpm) rotações por minutos")
-    }
-    
-    func deligaModoTurbo() {
-        print("O modo turbo está ligado? \(modoTurbo)")
-    }
-    
-    init(modoTurbo: Bool, tamanho: Double) {
-        self.modoTurbo = modoTurbo
-        self.tamanho = tamanho
-    }
-}
+var citroen: Citroen = Citroen(quantidadeDePortas: 4, sensorDere: true, fabricante: "Citroen")
+
+citroen.ligarMotor()
+print(citroen.logoCitroen)
+
+var pegeout: Pegeout = Pegeout(logoLeao: true, painel3D: true, revioesConcessionaria: "Green", quantidadeDePortas: 4, sensorDere: true, fabricante: "Pegeout")
+
+pegeout.acelerar()
